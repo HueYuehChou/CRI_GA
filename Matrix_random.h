1e7a0887e3e87dae9e27c0f634c42b8a57bd3e1c
HueYuehChou <101697659+HueYuehChou@users.noreply.github.com>
2022-03-21 18:25:26 +0800

    Add files via upload

--- Matrix_random.h
+++ Matrix_random.h
@@ -2,11 +2,11 @@
 constexpr int matrix_length=401;
 constexpr int matrix_width=3;
 
-float LED_wavelength_min=430, LED_wavelength_max=460; // 藍光LED波長範圍
+float LED_wavelength_min=430, LED_wavelength_max=460; // Blue light LED wavelength range
 
-float S_wavelength_min=520, S_wavelength_max=540;  // YAG短波長範圍
+float S_wavelength_min=520, S_wavelength_max=540;  // YAG short wavelength range
 
-float L_wavelength_min=520, L_wavelength_max=620;  // YAG長波長範圍
+float L_wavelength_min=520, L_wavelength_max=620;  // YAG long wavelength range
 
 float LED_Population[matrix_length][matrix_width];
 float S_Population[matrix_length][matrix_width];
@@ -25,7 +25,7 @@ float genes_range(float gene_min,float gene_max)
 
      float diff=gene_max-gene_min;
 
-     return (diff*rand()/RAND_MAX + gene_min); // 回傳範圍內的亂數
+     return (diff*rand()/RAND_MAX + gene_min); // feedback random values in interval between range.
 
 
 }
@@ -37,11 +37,11 @@ if(choice==0){
 
 for(int i=0;i<width;i++){
         for(int j=0;j<matrix_length;j++){
-                if(i==0)// blue light 波長
+                if(i==0)// Blue light LED wavelength
                 Population[j][i] = genes_range(LED_wavelength_min,LED_wavelength_max);
-                else if(i==1) // 左半高寬
+                else if(i==1) // Right FWHM
                 Population[j][i] = genes_range(Halfwidth_LED_min,LED_Population[j][0]-LED_wavelength_min);
-                else         //右半高寬
+                else         //Left FWHM
                 Population[j][i] = genes_range(Halfwidth_LED_min,LED_wavelength_max-LED_Population[j][0]);
         }
     }
@@ -51,9 +51,9 @@ else if (choice==1)
 {
         for(i=0;i<width;i++){
         for(j=0;j<matrix_length;j++){
-               if(i==0) // 短波長
+               if(i==0) // YAG short wavelength
                 Population[j][i] = genes_range(S_wavelength_min,S_wavelength_max);
-                else  //短波長半高寬
+                else  //FWHM
                 Population[j][i] = genes_range(Halfwidth_S_min,S_Population[j][0]-S_wavelength_min);
 
 }
@@ -63,9 +63,9 @@ return reinterpret_cast<float *>(Population);
 else{
     for(i=0;i<width;i++){
         for(j=0;j<matrix_length;j++){
-                if(i==0)// 長波長
+                if(i==0)// YAG long wavelength
                 Population[j][i] = genes_range(L_wavelength_min,L_wavelength_max);
-                else // 短波長半高寬
+                else // FWHM
                 Population[j][i] = genes_range(Halfwidth_L_min,L_Population[j][0]-L_wavelength_min);
 
 }
