a7bec20371f6a6b390dcb7d5d806711801ecbd23
HueYuehChou <101697659+HueYuehChou@users.noreply.github.com>
2022-03-26 09:01:42 +0800

    Delete excel_fileopen.h

--- excel_fileopen.h
+++
@@ -1,8 +0,0 @@
-#ifndef _EXCEL_FILEOPEN_H_
-#define _EXCEL_FILEOPEN_H_
-
-//宣告函數原型
-int CIE_Excelopen(int argc, const char * argv[],int interval,int CIE_1931_length,int CIE_1931_width,double temp[CIE_1931_length][CIE_1931_width]);
-
-
-#endif
