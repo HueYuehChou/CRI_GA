00d0400cf907a32f885be945656ff996a99aecbe
HueYuehChou <101697659+HueYuehChou@users.noreply.github.com>
2022-07-27 11:54:27 +0800

    Delete algo_220529/Gene_algo_0529 directory

--- algo_220529/Gene_algo_0529/Inverse_Matrix.h
+++
@@ -1,6 +0,0 @@
-#ifndef _INVERSE_MATRIX_
-#define _INVERSE_MATRIX_
-
-double matrix_inv(double* p, double* result);
-
-#endif
--- algo_220529/Gene_algo_0529/LM_Algorithm.h
+++
@@ -1,5 +0,0 @@
-#ifndef _LM_ALGORITHM_H_
-#define _LM_ALGORITHM_H_
-double LM_Algorithm(double Array1[9],double Tc,double Tc_uv_Slope_const_array[5],int TCS_width,int Lower_limit, int wavelength_interval, int data_length, int data_width,double CIE1931[data_length][data_width],double TCS[data_length][TCS_width],double Daylight_SPD_1nm[data_length][4]);
-
-#endif
